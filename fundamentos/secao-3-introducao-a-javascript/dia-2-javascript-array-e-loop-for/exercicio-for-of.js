3ffbbee6a9c9dc858bfc0f5eec03c2aaa2aeb658
Danillo Santos <dnashy@hotmail.com>
2023-01-24 16:08:15 -0300

    Conclui exercício 2 e 3 da Seção 3 - Dia 2

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-for-of.js
@@ -0,0 +1,28 @@
+// exercício 1
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    name = "Meu nome é " + name;
+    console.log(name);
+}
+
+
+// exercício 2
+
+let arrOfNumber = [10, 20, 30];
+
+for (let sum of arrOfNumber) {
+    console.log(sum);
+}
+
+
+// exercício 3 
+
+let arrOfNumbers = [10, 20, 30];
+
+for (let sum1 of arrOfNumbers) {
+    sum1 += 4;
+    console.log(sum1);
+}
+
