089655a416adc61e5482ea092bc754406684a02e
Danillo Santos <dnashy@hotmail.com>
2023-01-24 15:42:16 -0300

    Conclui primeiro exercício de fixação da Seção 3 - Dia 2

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-array.js
@@ -0,0 +1,26 @@
+// exercício 1 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+// exercício 2
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+
+// exercício 3
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu2.push('Contato');
+
+console.log(menu2);
+
+
+
