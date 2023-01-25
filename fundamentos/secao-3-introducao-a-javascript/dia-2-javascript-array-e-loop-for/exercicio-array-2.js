65de354400f0ad9f9e4c0d60434b026cca05a1ab
Danillo Santos <dnashy@hotmail.com>
2023-01-25 12:10:06 -0300

    Conclui exercícios de array e for

--- fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-array-2.js
+++ fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-array-2.js
@@ -73,18 +73,39 @@
 
 // exercício 6
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddNumbers = [];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let oddNumbers = [];
+// let result = 0;
 
-for (index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-        oddNumbers.push(numbers[index]);
-    }  
+// for (index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         result += 1;
+//         oddNumbers.push(numbers[index]);
+//     }
+// }
+
+// if (result === 0) {
+//   console.log("nenhum valor ímpar encontrado");
+// } else {
+//   console.log("temos " + result + " números ímpares");
+//   console.log(oddNumbers);
+// }
+
+
+// exercício 7
+
+let numbers = []
+let divisao = []
+
+for (let index = 1; index <= 25; index += 1) {
+    numbers.push(index);
 }
 
+console.log(numbers);
 
-console.log(oddNumbers);
-console.log("temos " + oddNumbers.length + " números ímpares");
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2)
+}
 
 
 
