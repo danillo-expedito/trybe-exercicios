35e2462b62b784d67f773ea73b248ce57c55f622
Danillo Santos <dnashy@hotmail.com>
2023-01-26 15:03:00 -0300

    Inicia resolução dos exercícios da Seção 3 - Dia 3

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-do-dia.js
@@ -0,0 +1,22 @@
+// exercício 1
+
+// let num = 10;
+// let sum;
+
+// for (let index = num - 1; index >= 1; index -= 1) {
+//     num = num * index
+// }
+
+// console.log(num);
+
+//exercício 2
+
+// let word = "tryber";
+// let invert = "";
+
+// for (index = word.length - 1; index >= 0; index -= 1) {
+//     invert = invert + word.charAt(index)
+// }
+
+// console.log(invert);
+
