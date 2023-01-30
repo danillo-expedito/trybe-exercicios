efb0bb07dcc631af8040e4f027c404e7ebc5378e
Danillo Santos <dnashy@hotmail.com>
2023-01-30 15:47:01 -0300

    Conclui exercícios bônus da Seção 3 - dia 5

--- fundamentos/secao-3-introducao-a-javascript/dia-5-javascript-arrow-functions-e-template-literals/exercicios-bonus.js
+++ fundamentos/secao-3-introducao-a-javascript/dia-5-javascript-arrow-functions-e-template-literals/exercicios-bonus.js
@@ -6,3 +6,17 @@ const substituaX = (nome) => {
 
 let firstName = 'Danillo';
 console.log(substituaX(firstName));
+
+
+// exercício 2
+
+const minhasSkills = (substituaX) => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+    let phrase = `${substituaX} Minhas três principais habilidades são:
+    ${skills[0]};
+    ${skills[1]};
+    ${skills[2]};`
+    return phrase;
+}
+
+console.log(minhasSkills(substituaX(firstName)));
