096f519d7142fc9966f18814c1c9b7798688f70b
Danillo Santos <dnashy@hotmail.com>
2023-01-30 15:22:37 -0300

    Conclui exercícios 3, 4 e 5 da Seção 3 - dia 5

--- fundamentos/secao-3-introducao-a-javascript/dia-5-javascript-arrow-functions-e-template-literals/exercicios-3-5.js
+++ fundamentos/secao-3-introducao-a-javascript/dia-5-javascript-arrow-functions-e-template-literals/exercicios-3-5.js
@@ -19,3 +19,33 @@ imprimeIdade()
   // Altere essa estrutura para corrigir o erro.
   console.log('Nome:', pessoa.nome);
   console.log('Idade:', pessoa.idade);
+
+
+// exercício 3
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
+
+
+// exercício 4 
+const firstName = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${firstName} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é = ${soma(a,b)}`);
+
+
+// exercício 5 - to arrow function
+// function numeroAleatorio() {
+//     return Math.random()
+//   }
+//   console.log(numeroAleatorio());
+
+const numeroAleatorio = () => Math.random();
+
+console.log(numeroAleatorio())
